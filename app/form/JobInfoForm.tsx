9867f3a11fee46a90474f73ef8712edc4be9fb13
yunji0387 <yunji0387@gmail.com>
2023-12-15 01:52:59 -0600

    Create Job Info Form

---
+++ app/form/JobInfoForm.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const JobInfoForm: React.FC = () => {
+  const [jobName, setJobName] = useState("");
+  const [customerName, setCustomerName] = useState("");
+
+  const handleJobNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setJobName(event.target.value);
+  };
+
+  const handleCustomerNameChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setCustomerName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const formData = {
+      jobName: jobName,
+      customerName: customerName,
+    };
+    console.log(JSON.stringify(formData));
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center bg-cyan-200 p-5 pt-2 space-y-2">
+      <h2 className="text-black text-center font-bold text-xl p-1">Job Info</h2>
+
+      <div className="w-80 flex flex-col justify-center">
+        <label htmlFor="jobName" className="text-black text-left w-full">
+          Job Name:
+        </label>
+        <input
+          type="text"
+          id="jobName"
+          value={jobName}
+          onChange={handleJobNameChange}
+          className="w-80 border border-black bg-light-gray text-black p-1 pl-2"
+        />
+      </div>
+
+      <div className="w-80 flex flex-col justify-center">
+        <label htmlFor="customerName" className="text-black text-left w-full">
+          Customer Name:
+        </label>
+        <select
+          id="customerName"
+          value={customerName}
+          onChange={handleCustomerNameChange}
+          className="w-80 p-1 border border-black bg-light-gray text-black"
+        >
+          <option value="customer1">Customer 1</option>
+          <option value="customer2">Customer 2</option>
+          <option value="customer3">Customer 3</option>
+        </select>
+      </div>
+
+      {/* <button
+        onClick={handleSubmit}
+        className="w-80 p-2 bg-blue-500 text-white"
+      >
+        Submit
+      </button> */}
+    </div>
+  );
+};
+
+export default JobInfoForm;
--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,3 +1,84 @@
-export default function Page() {
-    return <p>Form Page</p>
+// "use client";
+// import React, { useState } from 'react';
+
+// // Define a type for the customer data if needed
+// type Customer = {
+//     id: number;
+//     name: string;
+// };
+
+// const Form: React.FC = () => {
+//     const [jobName, setJobName] = useState<string>('');
+//     const [customerName, setCustomerName] = useState<string>('');
+
+//     // Replace this with real data and adjust the type accordingly
+//     const customers: Customer[] = [
+//         { id: 1, name: 'Customer 1' },
+//         { id: 2, name: 'Customer 2' },
+//         { id: 3, name: 'Customer 3' }
+//     ];
+
+//     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+//         e.preventDefault();
+//         // Handle your form submission logic here
+//         console.log({ jobName, customerName });
+//     };
+
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <div>
+//                 <label htmlFor="jobName">Job Name:</label>
+//                 <input
+//                     type="text"
+//                     id="jobName"
+//                     value={jobName}
+//                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setJobName(e.target.value)}
+//                 />
+//             </div>
+//             <div>
+//                 <label htmlFor="customerName">Customer Name:</label>
+//                 <select
+//                     id="customerName"
+//                     value={customerName}
+//                     onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setCustomerName(e.target.value)}
+//                 >
+//                     {customers.map(customer => (
+//                         <option key={customer.id} value={customer.name}>
+//                             {customer.name}
+//                         </option>
+//                     ))}
+//                 </select>
+//             </div>
+//             <button type="submit">Submit</button>
+//         </form>
+//     );
+// };
+
+// export default Form;
+
+"use client";
+import React from "react";
+import JobInfoForm from "./JobInfoForm";
+
+interface FormProps {
+  // Define your component props here
 }
+
+const Form: React.FC<FormProps> = ({}) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="flex flex-col items-center justify-around bg-white w-[25rem] min-h-[30rem] h-auto rounded-lg p-5">
+        <h1 className="w-96 text-black text-center font-bold text-3xl bg-lime-100 rounded-lg p-2">
+          Box Design Form
+        </h1>
+        <p className="w-90 text-black text-md p-3 text-justify bg-pink-100">
+          Please complete the form below and move to next step.
+        </p>
+        <JobInfoForm />
+        <button className="w-80 p-2 bg-dark-blue text-white rounded-lg">Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
