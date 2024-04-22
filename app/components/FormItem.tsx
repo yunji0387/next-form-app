c3c71173fe9039d1fb4b9af8440793f1d2da3df8
yunji0387 <yunji0387@gmail.com>
2024-04-22 01:00:32 -0500

    Refactor FormItem component to improve readability and consistency

--- app/components/FormItem.tsx
+++ app/components/FormItem.tsx
@@ -31,21 +31,21 @@ export default function FormItem({ formData }: FormItemProps) {
     formId,
   } = formData;
   return (
-    <div className="flex flex-col bg-gray-100 w-full p-2 m-2">
-      <p className="text-sm">Form ID: {formId}</p>
-      <p className="text-sm">Job Name: {jobName}</p>
-      <p className="text-sm">Customer Name: {customerName}</p>
-      <p className="text-sm">Material IDs: {materialID.join(", ")}</p>
-      <p className="text-sm">Material Names: {materialName.join(", ")}</p>
-      <p className="text-sm">Print Type: {printType}</p>
+    <div className="flex flex-col bg-gray-100 p-3 m-2">
+      <p className="text-sm"><span className="font-bold">Form ID:</span> {formId}</p>
+      <p className="text-sm"><span className="font-bold">Job Name:</span> {jobName}</p>
+      <p className="text-sm"><span className="font-bold">Customer Name:</span> {customerName}</p>
+      <p className="text-sm"><span className="font-bold">Material IDs:</span> {materialID.join(", ")}</p>
+      <p className="text-sm"><span className="font-bold">Material Names:</span> {materialName.join(", ")}</p>
+      <p className="text-sm"><span className="font-bold">Print Type:</span> {printType}</p>
       <p className="text-sm">
-        Print Customer Name: {printCustomerName ? "Yes" : "No"}
+      <span className="font-bold">Print Customer Name:</span> {printCustomerName ? "Yes" : "No"}
       </p>
       <p className="text-sm">
-        Print Custom Text: {printCustomText ? "Yes" : "No"}
+      <span className="font-bold">Print Custom Text:</span> {printCustomText ? "Yes" : "No"}
       </p>
-      <p className="text-sm">Custom Text: {customText}</p>
-      <p className="text-sm">Design Notes: {designNotes}</p>
+      <p className="text-sm"><span className="font-bold">Custom Text:</span> {customText}</p>
+      <p className="text-sm"><span className="font-bold">Design Notes:</span> {designNotes}</p>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import FormItem from "./components/FormItem";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { SubmissionErrorContent } from "./components/SubmissionErrorContent";
 
-
 // Define the structure of a form data object
 type FormData = {
   _id: string;
@@ -22,35 +21,39 @@ type FormData = {
 };
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [loadError, setLoadError] = useState<boolean>(false);
-  const [forms, setForms] = useState<FormData[]>([]); // State to store the form data
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadError, setLoadError] = useState(false);
+  const [forms, setForms] = useState<FormData[]>([]);
 
   useEffect(() => {
-    // const endpoint = process.env.NEXT_PUBLIC_FORM_SUBMISSION_URL;
-    const endpoint = "";
+    fetchForms();
+  }, []); // Dependency array is empty, so this effect will run only once after the component mounts
+
+  // Function to fetch forms
+  const fetchForms = async () => {
+    const endpoint = process.env.NEXT_PUBLIC_FORM_SUBMISSION_URL;
     if (!endpoint) {
       console.error("Submission endpoint is not defined.");
       setLoadError(true);
-      // alert("Submission endpoint is not set in the environment variables.");
       return;
     }
 
-    async function fetchForms() {
-      setIsLoading(true);
-      const response = await fetch(endpoint || ""); // Update with your endpoint
-      if (response.ok) {
-        const data: FormData[] = await response.json();
-        setForms(data); // Update state with fetched form data
-        setIsLoading(false);
-      } else {
-        console.error("Failed to fetch forms:", response.statusText);
-        setLoadError(true);
+    setIsLoading(true);
+    try {
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch forms: ${response.statusText}`);
       }
+      const data: FormData[] = await response.json();
+      setForms(data);
+      setLoadError(false); // Ensure error state is cleared on successful fetch
+    } catch (error) {
+      console.error("Error:", error);
+      setLoadError(true);
+    } finally {
+      setIsLoading(false);
     }
-    setIsLoading(false);
-    fetchForms();
-  }, []); // Empty dependency array means this effect runs once on mount
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-3 p-24">
@@ -61,34 +64,77 @@ export default function Home() {
           </button>
         </Link>
       </div>
-      <div className="flex flex-col gap-2 bg-white w-full h-[35rem] rounded-md">
-        <p className="w-full p-3 text-center font-bold text-2xl">
-          Form List
-        </p>
-        <div className="flex flex-col gap-2 overflow-auto">
-          {isLoading && <LoadingScreen text="Loading Form List ..." />}
-          {loadError && (
+      <div className="flex flex-col gap-2 bg-white w-full h-[35rem] rounded-md overflow-auto">
+        <p className="w-full p-3 text-center font-bold text-2xl">Form List</p>
+        {isLoading && <LoadingScreen text="Loading Form List..." />}
+        {loadError && (
           <SubmissionErrorContent
             headerText="Error Loading Form Data"
             contentText="An error occurred while loading the form data. Please try again later."
-            onRetry={() => {
-              setLoadError(false);
-            }}
+            onRetry={fetchForms} // Pass fetchForms to be called on retry
           />
         )}
-          {forms.map((form) => (
-            <FormItem key={form.formId} formData={form} />
-          ))}
-        </div>
+        {!isLoading &&
+          !loadError &&
+          forms.map((form) => <FormItem key={form.formId} formData={form} />)}
       </div>
     </main>
   );
 }
 
-// import Link from 'next/link';
-// import FormItem from './components/FormItem';
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import Link from "next/link";
+// import FormItem from "./components/FormItem";
+// import { LoadingScreen } from "./components/LoadingScreen";
+// import { SubmissionErrorContent } from "./components/SubmissionErrorContent";
+
+// // Define the structure of a form data object
+// type FormData = {
+//   _id: string;
+//   jobName: string;
+//   customerName: string;
+//   materialID: string[];
+//   materialName: string[];
+//   printType: string;
+//   printCustomerName: boolean;
+//   printCustomText: boolean;
+//   customText: string;
+//   designNotes: string;
+//   formId: number;
+// };
 
 // export default function Home() {
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+//   const [loadError, setLoadError] = useState<boolean>(false);
+//   const [forms, setForms] = useState<FormData[]>([]); // State to store the form data
+
+//   useEffect(() => {
+//     // const endpoint = process.env.NEXT_PUBLIC_FORM_SUBMISSION_URL;
+//     const endpoint = "";
+//     if (!endpoint) {
+//       console.error("Submission endpoint is not defined.");
+//       setLoadError(true);
+//       // alert("Submission endpoint is not set in the environment variables.");
+//       return;
+//     }
+
+//     async function fetchForms() {
+//       setIsLoading(true);
+//       const response = await fetch(endpoint || ""); // Update with your endpoint
+//       if (response.ok) {
+//         const data: FormData[] = await response.json();
+//         setForms(data); // Update state with fetched form data
+//         setIsLoading(false);
+//       } else {
+//         console.error("Failed to fetch forms:", response.statusText);
+//         setLoadError(true);
+//       }
+//     }
+//     setIsLoading(false);
+//     fetchForms();
+//   }, []); // Empty dependency array means this effect runs once on mount
+
 //   return (
 //     <main className="flex min-h-screen flex-col items-center justify-center gap-3 p-24">
 //       <div>
@@ -99,8 +145,24 @@ export default function Home() {
 //         </Link>
 //       </div>
 //       <div className="flex flex-col gap-2 bg-white w-full h-[35rem] rounded-md">
-//         <p className="bg-pink-100 w-full text-center font-bold text-2xl">Form List</p>
-//         <FormItem />
+//         <p className="w-full p-3 text-center font-bold text-2xl">
+//           Form List
+//         </p>
+//         <div className="flex flex-col gap-2 overflow-auto">
+//           {isLoading && <LoadingScreen text="Loading Form List ..." />}
+//           {loadError && (
+//           <SubmissionErrorContent
+//             headerText="Error Loading Form Data"
+//             contentText="An error occurred while loading the form data. Please try again later."
+//             onRetry={() => {
+//               setLoadError(false);
+//             }}
+//           />
+//         )}
+//           {forms.map((form) => (
+//             <FormItem key={form.formId} formData={form} />
+//           ))}
+//         </div>
 //       </div>
 //     </main>
 //   );
