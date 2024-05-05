dbec4f6aa5c5ac77b6c35eadeaf3a1859640479f
yunji0387 <yunji0387@gmail.com>
2024-05-05 17:26:46 -0500

    refactor: Update SideNav component layout and add logo

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -1,31 +1,72 @@
 "use client";
 import React from "react";
-import { ChevronLast, ChevronFirst } from "lucide-react";
+import { ChevronLast, ChevronFirst, MoreVertical } from "lucide-react";
 import Image from "next/image";
 
-export default function SideNav() {
+type SideNavProps = {
+  children: React.ReactNode;
+};
 
+export function SideBar({ children }: SideNavProps) {
   return (
     <aside className="h-screen">
-        <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-            <div className="p-4 pb-2 flex justify-between items-center">
-                <Image
-                    src= "https://img.logoipsum.com/243.svg"
-                    width={150}
-                    height={20}
-                    alt="logo"
-                />
-                <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
-                    <ChevronFirst size={24} />
-                </button>
-            </div>
+      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center">
+          <Image
+            src="https://img.logoipsum.com/243.svg"
+            width={150}
+            height={20}
+            alt="logo"
+          />
+          <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
+            <ChevronFirst size={24} />
+          </button>
+        </div>
 
-            
-        </nav>
+        <ul className="flex-1 px-3 bg-red-50">{children}</ul>
+
+        <div className="border-t flex p-3">
+          <Image
+            src="https://img.logoipsum.com/225.svg"
+            width={50}
+            height={50}
+            alt="avatar"
+          />
+          <div className="flex w-full items-center justify-between">
+            <div className="flex flex-col justify-between items-center w-full ml-3">
+              <h4 className="w-full font-semibold">John Doe</h4>
+              <span className="w-full text-xs text-gray">
+                johndoe@example.com
+              </span>
+            </div>
+            <MoreVertical size={24} />
+          </div>
+        </div>
+      </nav>
     </aside>
   );
 }
 
+export function SibebarItem({ icon, title, active, alert }: any) {
+  return (
+    <li
+      className={`relative flex items-center justify-start p-3 my-2 rounded-md font-medium cursor-pointer transition-colors
+        ${
+          active
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600"
+        }`}
+    >
+      {icon}
+      <span className="ml-3">{title}</span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400`}
+        />
+      )}
+    </li>
+  );
+}
 
 // "use client";
 // import React, { useState } from "react";
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import SideNav from "../components/SideNav";
+import { SideBar, SibebarItem } from "../components/SideNav";
+import {
+    LifeBuoy,
+    Receipt,
+    Boxes,
+    Settings,
+} from "lucide-react";
 
 export default function Dashboard() {
   return (
     <main className="flex w-full min-h-screen items-center justify-start gap-3">
       <div className="w-full">
-        <SideNav />
+        <SideBar>
+            <SibebarItem icon={<LifeBuoy />} title="Dashboard" active alert />
+            <SibebarItem icon={<Receipt />} title="Users" />
+            <SibebarItem icon={<Boxes />} title="Orders" alert />
+            <SibebarItem icon={<Settings />} title="Settings" />
+        </SideBar>
       </div>
       <div className="flex w-full">
         <h1 className="text-white">Dashboard</h1>
