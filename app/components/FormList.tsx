3ffb7e7d6021380590a9061e88296371814d0c41
yunji0387 <yunji0387@gmail.com>
2024-05-19 23:52:32 -0500

    refactor: Update FormList component to include rounded corners
    This commit updates the FormList component to include rounded corners in the button's styling. It aligns with recent user commits that focused on updating the Home page layout and improves the overall consistency of the application.

--- app/components/FormList.tsx
+++ app/components/FormList.tsx
@@ -91,10 +91,8 @@ export function FormList() {
       <ToastContainer />
       <div className="flex flex-col gap-2 bg-white dark:bg-gray-600 rounded-md w-full h-[35rem] p-3 overflow-auto">
         {verified && (
-          <div className="w-full flex flex-col">
-            <div className="w-full flex items-center justify-between">
-              <p className="font-bold text-3xl">Form List</p>
-            </div>
+          <div className="w-full flex items-center justify-between">
+            <p className="font-bold text-3xl">Form List</p>
             <div>
               <Link href="/form">
                 <button className="bg-indigo-500 dark:bg-emerald-500 hover:bg-indigo-700 dark:hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded">
