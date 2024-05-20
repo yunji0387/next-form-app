c0d66d32dbd0c13cddf1c07dbc24fa4ef9fe6c7b
yunji0387 <yunji0387@gmail.com>
2024-05-19 20:54:52 -0500

    refactor: Update FormList component to include rounded corners
    This commit updates the FormList component to include rounded corners in the button's styling. By adding the "rounded" class to the button's className, it ensures a consistent visual appearance throughout the application. This change aligns with recent user commits that focused on updating the Home page layout and improves the overall consistency of the application.

--- app/components/FormList.tsx
+++ app/components/FormList.tsx
@@ -97,7 +97,7 @@ export function FormList() {
             </div>
             <div>
               <Link href="/form">
-                <button className="bg-indigo-500 dark:bg-emerald-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-indigo-500 dark:bg-emerald-500 hover:bg-indigo-700 dark:hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded">
                   New Request
                 </button>
               </Link>
