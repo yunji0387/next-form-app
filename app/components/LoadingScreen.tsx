2c14bbfcace233770025b5683c7bbfeaa4412fda
yunji0387 <yunji0387@gmail.com>
2024-05-24 21:01:35 -0500

    refactor: Update FormList component to include loading screen color customization
    This commit updates the FormList component to include the ability to customize the color of the loading screen based on the current theme. It adds a new optional "color" prop to the LoadingScreen component and uses the "useTheme" hook from "next-themes" to determine the current theme. The color is set to white when the theme is dark and to #707070 when the theme is light. This change improves the visual consistency of the loading screen with the overall theme of the application.

--- app/components/LoadingScreen.tsx
+++ app/components/LoadingScreen.tsx
@@ -2,16 +2,17 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 type LoadingScreenProps = {
   text: string;
+  color?: string;
 };
 
-export function LoadingScreen({ text }: LoadingScreenProps) {
+export function LoadingScreen({ text, color }: LoadingScreenProps) {
   return (
     <div className="w-full h-[25rem] p-5 flex flex-col items-center justify-center">
       <p className="text-2xl font-extrabold mb-5">{text}</p>
       <ClipLoader
         size={150}
-        color={"#123abc"} // You can also pass color as a prop if needed
-        loading={true} // Ensure to pass the loading state if needed
+        color={ color ? color : "#123abc"}
+        loading={true}
         aria-label="Loading Spinner"
         data-testid="loader"
       />
