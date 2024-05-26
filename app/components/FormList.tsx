885f77c3c4af876ac01c356c508a2a6714cffe99
yunji0387 <yunji0387@gmail.com>
2024-05-26 00:03:07 -0500

    refactor: Update FormList component button styling for consistency
    This commit updates the button styling in the FormList component to ensure consistency throughout the application. The button now includes a gradient background color and hover effects, improving the visual appearance and user experience. This change aligns with recent user commits that focused on updating the Home page layout and improves the overall consistency of the application.

--- app/components/FormList.tsx
+++ app/components/FormList.tsx
@@ -97,7 +97,7 @@ export function FormList() {
             <p className="font-bold text-3xl">Form List</p>
             <div>
               <Link href="/form">
-                <button className="bg-indigo-500 dark:bg-emerald-500 hover:bg-indigo-700 dark:hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-gradient-to-tr from-indigo-200 to-indigo-100 hover:from-indigo-300 hover:to-indigo-200 dark:from-emerald-500 dark:to-emerald-700 hover:bg-indigo-700 dark:hover:bg-emerald-500 text-white font-bold py-2 px-4 rounded">
                   New Request
                 </button>
               </Link>
