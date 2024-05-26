8a73437a6a6379158b1ecbab02d5dd32c91e69e4
yunji0387 <yunji0387@gmail.com>
2024-05-25 22:23:58 -0500

    refactor: Update FormList component to include loading screen color customization

--- app/components/FormItem.tsx
+++ app/components/FormItem.tsx
@@ -62,41 +62,39 @@ export default function FormItem({ formData }: FormItemProps) {
         className="text-black dark:text-white w-full flex flex-col rounded-md bg-gray-100 hover:bg-gray-200 dark:bg-gray-500 dark:hover:bg-gray-400 cursor-pointer p-3 group"
         onClick={() => setIsExpanded(!isExpanded)}
       >
-        <div className="flex items-center justify-between h-6">
-          <p className="w-1/2 text-sm">
-            <span className="font-bold">Form ID:</span> {formId}
-          </p>
-          <div className="hidden group-hover:flex w-1/3 justify-end gap-2 select-none">
-            <button
-              className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleEdit();
-              }}
-            >
-              Edit
-            </button>
-            <button
-              className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
-              onClick={(e) => {
-                e.stopPropagation();
-                setIsExpanded(!isExpanded);
-              }}
-            >
-              {isExpanded ? "Collapse" : "Expand"}
-            </button>
-            <button
-              className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
-              onClick={(e) => {
-                e.stopPropagation();
-                setShowDeletePopup(true);
-                setIsExpanded(true);
-              }}
-            >
-              Delete
-            </button>
-          </div>
+        <div className="flex w-full justify-end gap-2 select-none">
+          <button
+            className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleEdit();
+            }}
+          >
+            Edit
+          </button>
+          <button
+            className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
+            onClick={(e) => {
+              e.stopPropagation();
+              setIsExpanded(!isExpanded);
+            }}
+          >
+            {isExpanded ? "Collapse" : "Expand"}
+          </button>
+          <button
+            className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
+            onClick={(e) => {
+              e.stopPropagation();
+              setShowDeletePopup(true);
+              setIsExpanded(true);
+            }}
+          >
+            Delete
+          </button>
         </div>
+        <p className="w-full text-sm">
+          <span className="font-bold">Form ID:</span> {formId}
+        </p>
         <p className="text-sm">
           <span className="font-bold">Job Name:</span> {jobName}
         </p>
@@ -134,15 +132,139 @@ export default function FormItem({ formData }: FormItemProps) {
           </>
         )}
         {showDeletePopup && (
-        <div className="w-full bg-gray-100 p-1 rounded-b-md">
-          <DeleteFormPopUp
-            formId={formData.formId}
-            onClose={() => setShowDeletePopup(false)}
-            setIsDeleted={setIsDeleted}
-          />
-        </div>
-      )}
+          <div className="w-full bg-gray-100 p-1 rounded-b-md">
+            <DeleteFormPopUp
+              formId={formData.formId}
+              onClose={() => setShowDeletePopup(false)}
+              setIsDeleted={setIsDeleted}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
 }
+
+// export default function FormItem({ formData }: FormItemProps) {
+//   const {
+//     jobName,
+//     customerName,
+//     materialID,
+//     materialName,
+//     printType,
+//     printCustomerName,
+//     printCustomText,
+//     customText,
+//     designNotes,
+//     formId,
+//     _id,
+//     __v,
+//   } = formData;
+
+//   const [isExpanded, setIsExpanded] = useState(false);
+//   const [showDeletePopup, setShowDeletePopup] = useState(false);
+//   const [isDeleted, setIsDeleted] = useState(false);
+//   const { currentFormData, setCurrentFormData } = useFormData();
+//   const router = useRouter();
+
+//   const handleEdit = () => {
+//     const { _id, __v, ...rest } = formData; // Destructure to exclude _id and __v
+//     setCurrentFormData(rest);
+//   };
+
+//   useEffect(() => {
+//     if (currentFormData?.formId === formData.formId) {
+//       router.push("/form"); // Perform navigation once the context data is updated
+//     }
+//   }, [currentFormData, router]);
+
+//   return (
+//     <div className={`${isDeleted ? "hidden" : "flex flex-col"} w-full`}>
+//       <div
+//         className="text-black dark:text-white w-full flex flex-col rounded-md bg-gray-100 hover:bg-gray-200 dark:bg-gray-500 dark:hover:bg-gray-400 cursor-pointer p-3 group"
+//         onClick={() => setIsExpanded(!isExpanded)}
+//       >
+//         <div className="flex items-center justify-between h-6">
+//           <p className="w-1/2 text-sm">
+//             <span className="font-bold">Form ID:</span> {formId}
+//           </p>
+//           <div className="hidden group-hover:flex w-1/3 justify-end gap-2 select-none">
+//             <button
+//               className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
+//               onClick={(e) => {
+//                 e.stopPropagation();
+//                 handleEdit();
+//               }}
+//             >
+//               Edit
+//             </button>
+//             <button
+//               className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
+//               onClick={(e) => {
+//                 e.stopPropagation();
+//                 setIsExpanded(!isExpanded);
+//               }}
+//             >
+//               {isExpanded ? "Collapse" : "Expand"}
+//             </button>
+//             <button
+//               className="text-gray-800 dark:text-gray-100 bg-gray-100 hover:bg-white dark:bg-gray-500 dark:hover:bg-gray-600 w-[4rem] min-w-[4rem] h-6 text-xs font-bold border border-gray-700 rounded"
+//               onClick={(e) => {
+//                 e.stopPropagation();
+//                 setShowDeletePopup(true);
+//                 setIsExpanded(true);
+//               }}
+//             >
+//               Delete
+//             </button>
+//           </div>
+//         </div>
+//         <p className="text-sm">
+//           <span className="font-bold">Job Name:</span> {jobName}
+//         </p>
+//         <p className="text-sm">
+//           <span className="font-bold">Customer Name:</span> {customerName}
+//         </p>
+
+//         {isExpanded && (
+//           <>
+//             <p className="text-sm">
+//               <span className="font-bold">Material IDs:</span>{" "}
+//               {materialID.join(", ")}
+//             </p>
+//             <p className="text-sm">
+//               <span className="font-bold">Material Names:</span>{" "}
+//               {materialName.join(", ")}
+//             </p>
+//             <p className="text-sm">
+//               <span className="font-bold">Print Type:</span> {printType}
+//             </p>
+//             <p className="text-sm">
+//               <span className="font-bold">Print Customer Name:</span>{" "}
+//               {printCustomerName ? "Yes" : "No"}
+//             </p>
+//             <p className="text-sm">
+//               <span className="font-bold">Print Custom Text:</span>{" "}
+//               {printCustomText ? "Yes" : "No"}
+//             </p>
+//             <p className="text-sm">
+//               <span className="font-bold">Custom Text:</span> {customText}
+//             </p>
+//             <p className="text-sm">
+//               <span className="font-bold">Design Notes:</span> {designNotes}
+//             </p>
+//           </>
+//         )}
+//         {showDeletePopup && (
+//         <div className="w-full bg-gray-100 p-1 rounded-b-md">
+//           <DeleteFormPopUp
+//             formId={formData.formId}
+//             onClose={() => setShowDeletePopup(false)}
+//             setIsDeleted={setIsDeleted}
+//           />
+//         </div>
+//       )}
+//       </div>
+//     </div>
+//   );
+// }
