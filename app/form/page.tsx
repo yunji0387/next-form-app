cd31e1f23d74ef401b0ed3800944751e2176e33e
yunji0387 <yunji0387@gmail.com>
2024-05-29 22:06:10 -0500

    refactor: Update BoxDesignForm component to improve code structure and responsiveness

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -213,7 +213,7 @@ export default function BoxDesignForm() {
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center bg-white border-black border-2 w-[25rem] md:w-[35rem] lg:w-[50rem] min-h-[30rem] max-h-[45rem] h-auto rounded-lg p-2">
+      <div className="flex flex-col items-center justify-center w-full">
         {isInitialLoading && <LoadingScreen text="Loading..." />}
         { verified && <>
           <div className="w-full grid grid-cols-8 items-center">
